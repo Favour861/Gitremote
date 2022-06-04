179880dec0f21515c24c893403b9e5b12d5faca7
Favour Afolabi <afolabifavouroluwatobi@gmail.com>
2022-06-04 13:06:31 +0100

    changed port number

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 
+var PORT = 7070;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
   res.end();
-}).listen(8080);
-0
-0
+}).listen(PORT);
+// 0
+// 0
